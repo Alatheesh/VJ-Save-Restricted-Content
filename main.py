d5298b34a073377708e36eb59bf968d0a0e7d936
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2024-01-27 07:54:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def progress(current, total, message, type):
 # start command
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**",
+	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**"
 	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/llathu63035")],[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/llathu63035")]]), reply_to_message_id=message.id)
 
 @bot.on_message(filters.command(["help"]))
